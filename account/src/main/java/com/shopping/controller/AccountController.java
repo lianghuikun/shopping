b35fa780f3e204eb17bb9e7bf63b27908872e09d
lianghuikun <906630008@qq.com>
2020-05-17 10:12:31 +0800

    分布式事务项目

---
+++ account/src/main/java/com/shopping/controller/AccountController.java
@@ -0,0 +1,28 @@
+package com.shopping.controller;
+
+import com.shopping.domain.CommonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.shopping.service.AccountService;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+
+@RestController
+@Slf4j
+public class AccountController {
+
+    @Resource
+    private AccountService accountService;
+
+    @PostMapping("account/decrease")
+    public CommonResult decrease(@RequestParam("userId") Long userId,
+                                 @RequestParam("money") BigDecimal money) {
+        accountService.decrease(userId, money);
+        return new CommonResult(200, "扣减账户余额成功");
+    }
+
+
+}
---
+++ account/src/main/java/com/shopping/service/impl/AccountServiceImpl.java
@@ -0,0 +1,30 @@
+package com.shopping.service.impl;
+
+import com.shopping.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import com.shopping.dao.AccountDao;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
+public class AccountServiceImpl implements AccountService {
+
+    @Resource
+    private AccountDao accountDao;
+
+    @Override
+    public void decrease(Long userId, BigDecimal money) {
+        log.info("********->account-service中扣减账户余额开始");
+        try {
+            TimeUnit.SECONDS.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        accountDao.decrease(userId, money);
+        log.info("********->account-service中扣减账户余额结束");
+    }
+}
